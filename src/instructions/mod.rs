ed627a37fa6da02371e9f5034ebdbcf002cc0477
Alexander Grey <alexgreyentropy@gmail.com>
2025-02-11 12:05:36 +0200

    Update mod.rs

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -12,3 +12,9 @@ pub use vault_instructions::*;
 
 pub mod token_vault;
 pub use token_vault::*;
+
+pub mod open_token_vault;
+pub mod split_token_vault;
+
+pub use open_token_vault::*;
+pub use split_token_vault::*;
