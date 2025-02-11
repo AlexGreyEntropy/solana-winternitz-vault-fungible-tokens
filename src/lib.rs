36f5119a622798ae701583ecad38d373a26fbff4
Alexander Grey <alexgreyentropy@gmail.com>
2025-02-11 13:35:47 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,44 @@
+use anchor_lang::prelude::*;
+
+pub mod error;
 pub mod instructions;
+pub mod state;
+pub mod utils;
+
 use instructions::*;
 
-#[cfg(test)]
-pub mod tests;
+declare_id!("WNTZVaLmV6TaLWRKxhJgQL8HYzLwUFb5Bf1MJ5wZBVh");
 
-use pinocchio::{
-    account_info::AccountInfo, entrypoint, program_error::ProgramError, pubkey::Pubkey,
-    ProgramResult,
-};
+#[program]
+pub mod solana_winternitz_vault {
+    use super::*;
+    
+    pub fn open(ctx: Context<Open>, pubkey_root: [u8; 32]) -> Result<()> {
+        instructions::open::handle_open(ctx, pubkey_root)
+    }
 
-// wntrRTssxbf1rz9RPJ4xNBbpXxfsidQJT177NN3MXhB
-pub const ID: Pubkey = [
-    0x0e, 0x09, 0x41, 0x96, 0x88, 0xf6, 0x50, 0xcd, 0xb0, 0x5d, 0x81, 0x14, 0x81, 0xe4, 0x9a, 0x03,
-    0x4a, 0xd6, 0xb9, 0x89, 0x00, 0x31, 0x23, 0x65, 0x90, 0xb0, 0xef, 0x63, 0x98, 0x19, 0x46, 0xf2,
-];
+    pub fn split(
+        ctx: Context<Split>,
+        amount: u64,
+        signature: Vec<u8>,
+        message: Vec<u8>,
+    ) -> Result<()> {
+        instructions::split::handle_split(ctx, amount, signature, message)
+    }
 
-entrypoint!(process_instruction);
+    pub fn open_token_vault(
+        ctx: Context<OpenTokenVault>,
+        pubkey_root: [u8; 32],
+    ) -> Result<()> {
+        instructions::open_token_vault::handle_open_token_vault(ctx, pubkey_root)
+    }
 
-fn process_instruction(
-    _program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
-    let (discriminator, data) = instruction_data
-        .split_first()
-        .ok_or(ProgramError::InvalidInstructionData)?;
-    match VaultInstructions::try_from(discriminator)? {
-        VaultInstructions::SplitVault => SplitVault::deserialize(data)?.process(accounts),
-        VaultInstructions::OpenVault => OpenVault::deserialize(data)?.process(accounts),
-        VaultInstructions::CloseVault => CloseVault::deserialize(data)?.process(accounts),
+    pub fn split_token_vault(
+        ctx: Context<SplitTokenVault>,
+        amount: u64,
+        signature: Vec<u8>,
+        message: Vec<u8>,
+    ) -> Result<()> {
+        instructions::split_token_vault::handle_split_token_vault(ctx, amount, signature, message)
     }
 }
